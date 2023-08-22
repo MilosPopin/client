00605ccf2631ec847792fabe6cebb1a9c3b06bd2
SIMCON-VM\Creen <Milo Stankovi>
2023-08-22 19:07:54 +0200

    End of sectio 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,11 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DateInputComponent } from './_forms/date-input/date-input.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directive/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 
 
@@ -51,6 +56,11 @@ import { MemberMessagesComponent } from './members/member-messages/member-messag
     TextInputComponent,
     DateInputComponent,
     MemberMessagesComponent,
+    AdminPanelComponent,
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent,
     
   ],
   imports: [
@@ -71,7 +81,7 @@ import { MemberMessagesComponent } from './members/member-messages/member-messag
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
 
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 
